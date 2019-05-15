ad94acec61027a7bc7dd9fc663b381a1c4b019f2
github username <github email address>
2019-05-15 13:21:13 -0700

    routing to major components complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Welcome from "./components/Welcome.js";
 import Homepage from "./components/Homepage.js";
+import EditAccount from "./components/EditAccount.js";
+import SearchInfluencers from "./components/SearchInfluencers.js";
+import SearchBooks from "./components/SearchBooks.js";
+import Profile from "./components/Profile.js";
+import MyBookList from "./components/MyBookList.js";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Welcome />
-          <Homepage />
-        </header>
-      </div>
+      <Router>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/homepage" component={Homepage} />
+        <Route exact path="/editaccount" component={EditAccount} />
+        <Route exact path="/searchinfluencers" component={SearchInfluencers} />
+        <Route exact path="/searchbooks" component={SearchBooks} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/mybooklist" component={MyBookList} />
+      </Router>
     );
   }
 }
