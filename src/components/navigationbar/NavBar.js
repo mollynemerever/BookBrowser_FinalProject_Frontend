3581fea967f336ceb7e02df8688b6012d9da8fbb
github username <github email address>
2019-05-29 16:09:25 -0700

    front page styled and nav bar font size larger

--- src/components/navigationbar/NavBar.js
+++ src/components/navigationbar/NavBar.js
@@ -7,11 +7,6 @@ import "semantic-ui-css/semantic.min.css";
 import { Menu } from "semantic-ui-react";
 
 class NavBar extends Component {
-  // handleItemClick = (e, { name }) => {
-  //   this.setState({ active: name });
-  //   window.localStorage.setItem("active", JSON.stringify(name));
-  // };
-
   render() {
     const activeItem = JSON.parse(window.localStorage.getItem("active"));
     let greeting;
